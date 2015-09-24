4addfcc08fd4f6fb8327d028a8e3573e94595166
idtteam <hashidthash@gmail.com>
2015-09-24 07:18:39 +0200

    Add some untested values as I don't know how to test yet

--- new_asset/models.py
+++ new_asset/models.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from account_asset import account_asset_depreciation_line
 
-# class new_asset(models.Model):
-#     _name = 'new_asset.new_asset'
-
-#     name = fields.Char()
+ #class new_asset_test1(models.Model):
+ #    _name = 'new_asset.new_asset'
+ #    name = fields.Char()
+ #    test1 = account_asset_depreciation_line.asset_id
+ 
+class new_asset_test2(models.Model):
+    _inherit = 'account_asset.account_asset_depreciation_line'
+    _columns = {
+        'asset_date_test2_id': depreciation_date,
+    }
